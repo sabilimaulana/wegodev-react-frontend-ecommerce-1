23dbd942a68374bf94dcf9e9bd816f70f550e5fa
sabilimaulana <spacysbl.learn@gmail.com>
2021-08-23 09:46:23 +0700

    Make Header component

--- modules/common/components/Layout/index.js
+++ modules/common/components/Layout/index.js
@@ -1,5 +1,6 @@
 import { makeStyles } from "@material-ui/core/styles";
 import PropTypes from "prop-types";
+import MobileHeader from "../../../mobile/components/Header";
 
 const useStyles = makeStyles({
   content: {
@@ -11,6 +12,7 @@ const Layout = ({ children }) => {
   const classes = useStyles();
   return (
     <>
+      <MobileHeader />
       <div className={classes.content}>{children}</div>
     </>
   );
