f076094007d943980352d74f29e2279b06ec6731
sabilimaulana <spacysbl.learn@gmail.com>
2021-08-24 09:44:56 +0700

    ProductDetailCard part 2

--- modules/mobile/components/ProductDetailCard/index.js
+++ modules/mobile/components/ProductDetailCard/index.js
@@ -1,8 +1,16 @@
 import PropTypes from "prop-types";
 import clsx from "clsx";
 
-import { Card, CardContent, CardMedia, Typography } from "@material-ui/core";
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
+import { currencyFormatter } from "@utils/currency";
+import ProductRating from "@common/components/ProductRating";
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -11,6 +19,9 @@ const useStyles = makeStyles((theme) => ({
   media: {
     height: 0,
     paddingTop: "56.25%",
+  },
+  productInfo: {
+    marginTop: theme.spacing(2),
   },
 }));
 
@@ -37,6 +48,83 @@ const ProductDetailCard = ({
           <Typography variant="h5" component="h1" gutterBottom>
             {title}
           </Typography>
+
+          <Grid container spacing={1}>
+            <Grid item xs={6} sm={6}>
+              <Typography variant="button" color="secondary">
+                {currencyFormatter(price)}
+              </Typography>
+            </Grid>
+            <Grid item xs={6} sm={6}>
+              <ProductRating rating={rating} sold={sold} alignRight />
+            </Grid>
+          </Grid>
+
+          <Grid container spacing={1} className={classes.productInfo}>
+            <Grid
+              container
+              direction="column"
+              justifyContent="center"
+              item
+              xs={3}
+              sm={3}
+            >
+              <Typography align="center" variant="overline">
+                Berat
+              </Typography>
+              <Typography align="center" variant="button">
+                {weight}
+              </Typography>
+            </Grid>
+
+            <Grid
+              container
+              direction="column"
+              justifyContent="center"
+              item
+              xs={3}
+              sm={3}
+            >
+              <Typography align="center" variant="overline">
+                Kondisi
+              </Typography>
+              <Typography align="center" variant="button">
+                {condition}
+              </Typography>
+            </Grid>
+
+            <Grid
+              container
+              direction="column"
+              justifyContent="center"
+              item
+              xs={3}
+              sm={3}
+            >
+              <Typography align="center" variant="overline">
+                Stock
+              </Typography>
+              <Typography align="center" variant="button">
+                {quantity}
+              </Typography>
+            </Grid>
+
+            <Grid
+              container
+              direction="column"
+              justifyContent="center"
+              item
+              xs={3}
+              sm={3}
+            >
+              <Typography align="center" variant="overline">
+                Terjual
+              </Typography>
+              <Typography align="center" variant="button">
+                {sold}
+              </Typography>
+            </Grid>
+          </Grid>
         </CardContent>
       </Card>
     </div>
